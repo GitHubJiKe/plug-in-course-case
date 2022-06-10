2f6869bbf1ced3cecbca8faf0f6f0f5082cf1b18
pengfeiyuan <ypf9319@gmail.com>
2022-06-11 01:03:03 +0800

    modify config

--- ad-recommendation/esbuildConfig.js
+++ ad-recommendation/esbuildConfig.js
@@ -1,49 +1,46 @@
-const path = require("path")
-const sassPlugin = require('esbuild-plugin-sass')
-const { htmlPlugin } = require('@craftamap/esbuild-plugin-html');
-const purgecssPlugin = require("esbuild-plugin-purgecss")
-const pkg = require("./package.json")
-const fs = require("fs")
+const path = require("path");
+const sassPlugin = require("esbuild-plugin-sass");
+const { htmlPlugin } = require("@craftamap/esbuild-plugin-html");
+const purgecssPlugin = require("esbuild-plugin-purgecss");
+const pkg = require("./package.json");
 
 module.exports = {
     entryPoints: [path.resolve(__dirname, "./src/index.tsx")],
     outdir: path.resolve(__dirname, "./dist"),
     platform: "browser",
     target: ["es2020", "chrome58"],
-    loader: { '.js': 'tsx' },
+    loader: { ".js": "tsx" },
     bundle: true,
     minify: true,
     minifyWhitespace: true,
     minifyIdentifiers: true,
     minifySyntax: true,
     metafile: true,
-    plugins: [sassPlugin(), purgecssPlugin(), htmlPlugin({
-        files: [
-            {
-                entryPoints: [path.resolve(__dirname, "./src/index.tsx")],
-                filename: "index.html",
-                title: pkg.name,
-                htmlTemplate: fs.readFileSync(path.resolve(__dirname, './index.html'))
-            }
-        ]
-    }), MyHtmlPlugin({ filepath: path.resolve(__dirname, './index.html'), filename: 'index.html' })]
-}
-
-function MyHtmlPlugin({ filepath, filename }) {
-    return {
-        name: 'myhtml',
-        setup(build) {
-            build.onStart(() => {
-                const htmlContent = fs.readFileSync(filepath)
-                const distDir = build.initialOptions.outdir
-                const _file_path_ = `${build.initialOptions.outdir}/${filename}`;
-                try {
-                    fs.accessSync(distDir)
-                } catch (error) {
-                    fs.mkdirSync(build.initialOptions.outdir)
-                }
-                fs.writeFileSync(_file_path_, htmlContent)
-            })
-        }
-    }
-}
+    plugins: [
+        sassPlugin(),
+        purgecssPlugin(),
+        htmlPlugin({
+            files: [
+                {
+                    entryPoints: [path.resolve(__dirname, "./src/index.tsx")],
+                    filename: "index.html",
+                    title: pkg.name,
+                    htmlTemplate: `<!DOCTYPE html>
+                    <html lang="en">
+                        <head>
+                            <meta charset="UTF-8" />
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                            <link rel="stylesheet" href="/index.css" />
+                        </head>
+                    
+                        <body>
+                            <div id="root" class="padding-12px bg-black"></div>
+                            <script src="/index.js"></script>
+                        </body>
+                    </html>
+                    `,
+                },
+            ],
+        }),
+    ],
+};
--- ad-recommendation/serve.js
+++ ad-recommendation/serve.js
@@ -1,6 +1,13 @@
-const esbuild = require("esbuild")
-const esbuildConfig = require("./esbuildConfig")
+const esbuild = require("esbuild");
+const esbuildConfig = require("./esbuildConfig");
 
 esbuild
-    .serve({ port: 9000, host: "localhost", servedir: "./dist" }, esbuildConfig)
-    .then(() => console.log("http://localhost:9000"))
+    .serve(
+        {
+            port: 9000,
+            host: "localhost",
+            servedir: "./",
+        },
+        esbuildConfig,
+    )
+    .then(() => console.log("http://localhost:9000"));
